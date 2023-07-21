b7c07547d2dd0d2d3a961216507791b825487252
Nabeel Ahmed <72853909+nabeelahmedjh@users.noreply.github.com>
2023-07-21 12:35:44 +0500

    added registration template and route

--- django_login_register/accounts/views.py
+++ django_login_register/accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def registerUser(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        pass
+
+    if request.method == 'GET':
+        context = {'form': form}
+
+    
+
+    return render(request, 'accounts/register.html', context)
